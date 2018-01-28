5c49f9d70131ad255f06527eb1bb50f0b3e35396
Chris Humphrey <chrishumphrey1985@hotmail.co.uk>
2018-01-28 19:25:26 +0000

    Created Bank Accounts, Transactions and Tests

---
+++ BankAccount/Account.cs
@@ -0,0 +1,52 @@
+﻿using BankAccount.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class Account : IAccount
+    {
+        private List<ITransaction>  _Transactions { get; set; }
+        public string Name { get; protected set; }
+
+        public Account(string name)
+        {
+            Name = name;
+
+            _Transactions = new List<ITransaction>();
+        }
+
+
+
+        /// <summary>
+        /// Method to add a transaction to the bank account
+        /// </summary>
+        /// <param name="transaction">The transaction to add</param>
+        public void AddTransaction(ITransaction transaction)
+        {
+            _Transactions.Add(transaction);
+        }
+
+        /// <summary>
+        /// Retrieve the current balance for the bank account
+        /// </summary>
+        /// <returns>The Current Balance</returns>
+        public decimal GetCurrentBalance()
+        {
+            if (_Transactions.Count == 0)
+                return 0;
+            else
+                return _Transactions.Sum(x => x.Amount);
+        }
+
+        /// <summary>
+        /// Represents the transactions held in the bank account, read-only status stops list being modified outside of this class.
+        /// </summary>
+        public IReadOnlyCollection<ITransaction> Transactions { get { return _Transactions.AsReadOnly(); } }
+
+    }
+}
---
+++ BankAccountTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankAccountTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankAccountTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa3e50cb-6d79-4c63-9ab4-03d5f665cd6a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
